4d2c293e705b1436f42ab87da710fc42b4cc84dd
Julien Froger <webdev.julienfroger@gmail.com>
2023-06-12 12:06:18 +0200

    Add: first commit all files

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require( 'dotenv' );
+dotenv.config({ path: './.env' });
+
+const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+
+app.use(express.json());
+
+mongoose
+.connect(process.env.MONGO_URI,
+    { useNewUrlParser: true,
+    useUnifiedTopology: true })
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Mettez le domaine autorisé à accéder à l'API au lieu de "*"
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
+app.use( '/stuff', productRoutes );
+app.use( '/auth', userRoutes );
+
+module.exports = app;
---
+++ backend/controllers/productController.js
@@ -0,0 +1,40 @@
+const Product = require( '../models/Product' );
+
+exports.createProduct = ( req, res, next ) => {
+    const productObject = JSON.parse(req.body.thing);
+    delete productObject._id;
+    delete productObject.user_id;
+    const product = new Product({
+        ...productObject,
+        userId: req.auth.userId,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    });
+    product.save()
+    .then(() => res.status(200).json({ message: 'Objet enregistré !' }))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.updateProduct = (req, res, next) => {
+    Product.updateOne({ _id: req.params.id}), {...req.body, _id: req.params.id}
+    .then(() => res.statut(200).json({ message: 'Objet modifié !' }))
+    .catch(error => res.status(400).json({ error }))
+};
+
+exports.deleteProduct = (req, res, next) => {
+    Product.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.oneProduct = (req, res, next) => {
+    Product.findOne({ _id: req.params.id })
+    .then(product => res.status(200).json({ product }))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.allProduct = (req, res, next) => {
+    Product.find()
+    .then(product => res.status(200).json(product))
+    .catch(error => res.status(400).json({ error }));
+};
+

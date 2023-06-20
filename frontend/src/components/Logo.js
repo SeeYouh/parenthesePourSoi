17bb66925354b5d0292c2dabc06f41ff5052412e
Julien Froger <webdev.julienfroger@gmail.com>
2023-06-21 00:04:32 +0200

    add: Logo file & components

---
+++ frontend/src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div id="bgLogo">
+      <div id="leafBottomLeft"></div>
+      <div id="logoPicture"></div>
+      <div id="leafBottomRight"></div>
+    </div>
+  );
+};
+
+export default Logo;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navigation from "./../components/Navigation";
 import Card from "../components/data/ProductCard";
+import Logo from "../components/Logo";
 
 const Home = () => {
   return (
     <div>
+      <Logo />
       <Navigation />
       <Card />
     </div>

c278d9bd39f2d5d629ef6add5952837428ef861d
Julien Froger <webdev.julienfroger@gmail.com>
2023-06-20 12:00:55 +0200

    Delete: all files & function authentication

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Navigation from "./../components/Navigation";
-// import Product from "./../components/data/Product";
 import Card from "../components/data/CardProduct";
 
 const Home = () => {

2d2d1625878549d68bff7782f263bce23bbc9ad9
Julien Froger <webdev.julienfroger@gmail.com>
2023-06-20 11:52:32 +0200

    Delete: file Product.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Navigation from "./../components/Navigation";
-import Product from "./../components/data/Product";
+// import Product from "./../components/data/Product";
+import Card from "../components/data/CardProduct";
 
 const Home = () => {
   return (
     <div>
       <Navigation />
-      <Product />
+      <Card />
     </div>
   );
 };

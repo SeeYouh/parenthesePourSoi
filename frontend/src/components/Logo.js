41319019864ade1924b0c65c7bc527a0f68bcce2
Julien Froger <webdev.julienfroger@gmail.com>
2023-06-21 15:24:41 +0200

    update: animation btn product, delete various useless files

--- frontend/src/components/Logo.js
+++ frontend/src/components/Logo.js
@@ -3,9 +3,18 @@ import React from "react";
 const Logo = () => {
   return (
     <div id="bgLogo">
-      <div id="leafBottomLeft"></div>
-      <div id="logoPicture"></div>
-      <div id="leafBottomRight"></div>
+      <div id="leafBottomLeft">
+        <img src="./img/svg/leafLeft.svg" alt="Illustration de feuille" />
+      </div>
+      <div id="logoPicture">
+        <img
+          src="./img/svg/Logo une parenthèse pour soi-min.svg"
+          alt="Logo d'Une parenthèse pour Soi"
+        />
+      </div>
+      <div id="leafTopRight">
+        <img src="./img/svg/leafRight.svg" alt="Illustration de feuille" />
+      </div>
     </div>
   );
 };

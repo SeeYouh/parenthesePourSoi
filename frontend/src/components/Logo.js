97e3fd29736afbd13b954cf0bea20f6078e72490
Julien Froger <webdev.julienfroger@gmail.com>
2023-06-26 09:56:46 +0200

    test: parallax reset

--- frontend/src/components/Logo.js
+++ frontend/src/components/Logo.js
@@ -1,39 +1,7 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { generalDataImg } from "../data/generalData";
 
 const Logo = () => {
-  // let leafTopRight = document.getElementById(leafTopRight);
-  // let leafBottomLeft = document.getElementById(leafBottomLeft);
-  // let logoPicture = document.getElementById(logoPicture);
-
-  // window.addEventListener("scroll", () => {
-  //   let value = window.scrollY;
-
-  //   leafTopRight.style.bottom = value * 1.5 + "px";
-  //   leafBottomLeft.style.bottom = value * 1.5 + "px";
-  //   logoPicture.style.bottom = value * 3.5 + "px";
-  // });
-  const leafTopRightRef = useRef(null);
-  const leafBottomLeftRef = useRef(null);
-  const logoPictureRef = useRef(null);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      let value = window.scrollY;
-
-      leafTopRightRef.current.style.bottom = value * 1.5 + "px";
-      leafBottomLeftRef.current.style.bottom = value * 1.5 + "px";
-      logoPictureRef.current.style.bottom = value * 3.5 + "px";
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    // Cleanup function to remove the event listener when the component unmounts
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <div id="bgLogo">
       <div>
@@ -57,16 +25,15 @@ const Logo = () => {
             </div>
           </div>
         </div>
-        <img
-          id="logoPicture"
-          key={generalDataImg[0].id}
-          src={generalDataImg[0].src}
-          alt={generalDataImg[0].alt}
-        />
+        <div>
+          <img
+            id="logoPicture"
+            key={generalDataImg[0].id}
+            src={generalDataImg[0].src}
+            alt={generalDataImg[0].alt}
+          />
+        </div>
       </div>
-      <div id="logoPicture"></div>
-      <div id="leafBottomLeft"></div>
-      <div></div>
     </div>
   );
 };

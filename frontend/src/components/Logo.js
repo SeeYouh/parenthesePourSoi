756fa55ab7a77a10c10d68c25c7131850cc8a0ac
Julien Froger <webdev.julienfroger@gmail.com>
2023-06-26 03:59:32 +0200

    update: css navbar & logoPicture

--- frontend/src/components/Logo.js
+++ frontend/src/components/Logo.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { generalDataImg } from "../data/generalData";
 
 const Logo = () => {
+  // let leafTopRight = document.getElementById(leafTopRight);
+  // let leafBottomLeft = document.getElementById(leafBottomLeft);
+  // let logoPicture = document.getElementById(logoPicture);
+
+  // window.addEventListener("scroll", () => {
+  //   let value = window.scrollY;
+
+  //   leafTopRight.style.bottom = value * 1.5 + "px";
+  //   leafBottomLeft.style.bottom = value * 1.5 + "px";
+  //   logoPicture.style.bottom = value * 3.5 + "px";
+  // });
+  const leafTopRightRef = useRef(null);
+  const leafBottomLeftRef = useRef(null);
+  const logoPictureRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      let value = window.scrollY;
+
+      leafTopRightRef.current.style.bottom = value * 1.5 + "px";
+      leafBottomLeftRef.current.style.bottom = value * 1.5 + "px";
+      logoPictureRef.current.style.bottom = value * 3.5 + "px";
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    // Cleanup function to remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div id="bgLogo">
-      <div className="positionFixed">
+      <div>
         <div id="leaf">
           <div id="leafBottomLeft">
             <div id="leafBottomLeftImg">
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Navigation from "./../components/Navigation";
 import Card from "../components/data/ProductCard";
 
 const Home = () => {
   return (
     <div data-scroll-section>
       <div className="bgWhite">
-        <Navigation />
         <Card />
       </div>
     </div>

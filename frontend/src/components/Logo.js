d751d7f36d808559c50579b91c7341547aed77fa
Julien Froger <webdev.julienfroger@gmail.com>
2023-07-04 16:01:45 +0200

    corriger(Logo.js) : ajouter l'importation manquante de "framer-motion" et utiliser la fonction useScroll pour obtenir la valeur de défilement fonctionnalité(Logo.js) : ajouter une barre de progression qui se met à l'échelle en fonction de la valeur de défilement ajouter(Parallax.js) : ajouter un nouveau composant qui utilise l'effet de parallaxe lors du défilement modifier(_variable.scss) : ajouter une classe .progressBar pour styliser la barre de progression

--- frontend/src/components/Logo.js
+++ frontend/src/components/Logo.js
@@ -1,40 +1,49 @@
 import React from "react";
 import { generalDataImg } from "../data/generalData";
+import { motion, useScroll } from "framer-motion";
 
 const Logo = () => {
+  const scrollYProgress = useScroll();
+
   return (
-    <div id="bgLogo">
-      <div>
-        <div id="leaf">
-          <div id="leafBottomLeft">
-            <div id="leafBottomLeftImg">
-              <img
-                key={generalDataImg[1].id}
-                src={generalDataImg[1].src}
-                alt={generalDataImg[1].alt}
-              />
-            </div>
-          </div>
-          <div id="leafTopRight">
-            <div id="leafTopRightImg">
-              <img
-                key={generalDataImg[2].id}
-                src={generalDataImg[2].src}
-                alt={generalDataImg[2].alt}
-              />
-            </div>
-          </div>
-        </div>
+    <>
+      <motion.div
+        className="progressBar"
+        style={{ scaleX: scrollYProgress }}
+      ></motion.div>
+      <div id="bgLogo">
         <div>
-          <img
-            id="logoPicture"
-            key={generalDataImg[0].id}
-            src={generalDataImg[0].src}
-            alt={generalDataImg[0].alt}
-          />
+          <div id="leaf">
+            <div id="leafBottomLeft">
+              <div id="leafBottomLeftImg">
+                <img
+                  key={generalDataImg[1].id}
+                  src={generalDataImg[1].src}
+                  alt={generalDataImg[1].alt}
+                />
+              </div>
+            </div>
+            <div id="leafTopRight">
+              <div id="leafTopRightImg">
+                <img
+                  key={generalDataImg[2].id}
+                  src={generalDataImg[2].src}
+                  alt={generalDataImg[2].alt}
+                />
+              </div>
+            </div>
+          </div>
+          <div>
+            <img
+              id="logoPicture"
+              key={generalDataImg[0].id}
+              src={generalDataImg[0].src}
+              alt={generalDataImg[0].alt}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ frontend/src/components/data/framer-motion/Parallax.js
@@ -0,0 +1,49 @@
+// Importation des modules nécessaires
+import { useRef } from "react";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+
+// Cette fonction utilise la valeur de défilement pour créer un effet de parallaxe.
+// Elle transforme la valeur de défilement (entre 0 et 1) en une valeur de déplacement (entre -distance et distance).
+function useParallax(value, distance) {
+  return useTransform(value, [0, 1], [-distance, distance]);
+}
+
+// Ce composant Image affiche une image et un titre qui se déplace avec un effet de parallaxe lors du défilement.
+function Image({ id }) {
+  // useRef est utilisé pour obtenir une référence à l'élément DOM qui sera utilisé pour le défilement.
+  const ref = useRef(null);
+  // useScroll est utilisé pour obtenir la valeur de défilement de l'élément référencé.
+  const { scrollYProgress } = useScroll({ target: ref });
+  // useParallax est utilisé pour obtenir la valeur de déplacement pour l'effet de parallaxe.
+  const y = useParallax(scrollYProgress, 840);
+
+  return (
+    <section>
+      <div ref={ref}>
+        <img src={`/${id}.jpg`} alt="Un gratte-ciel de Londres" />
+      </div>
+      <motion.h2 style={{ y }}>{`#00${id}`}</motion.h2>
+    </section>
+  );
+}
+
+// Ce composant App affiche plusieurs images avec un effet de parallaxe et une barre de progression pour le défilement.
+export default function App() {
+  // useScroll est utilisé pour obtenir la valeur de défilement de la fenêtre.
+  const { scrollYProgress } = useScroll();
+  // useSpring est utilisé pour créer un effet de ressort pour la barre de progression.
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+
+  return (
+    <>
+      {[1, 2, 3, 4, 5].map((image) => (
+        <Image id={image} key={image} />
+      ))}
+      <motion.div className="progress" style={{ scaleX }} />
+    </>
+  );
+}

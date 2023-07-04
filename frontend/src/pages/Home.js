d43dccba0d90de882b9c267a9fd466adeb6a93e4
Julien Froger <webdev.julienfroger@gmail.com>
2023-07-05 01:10:48 +0200

    corriger(Parallax.js) : changer la valeur de déplacement pour l'effet de parallaxe de 840 à 50 fonctionnalité(Parallax.js) : changer l'image source pour afficher une image de feuille corriger(Home.js) : importer le composant Parallax fonctionnalité(Home.js) : ajouter le composant Parallax à la page d'accueil

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Card from "../components/data/ProductContainer";
 import Logo from "../components/Logo";
 import { motion, useScroll, useSpring } from "framer-motion";
+import Parallax from "../components/data/framer-motion/Parallax";
 
 const Home = () => {
   const { scrollYProgress } = useScroll();
@@ -14,6 +15,7 @@ const Home = () => {
     <>
       <motion.div className="progressBar" style={{ scaleX }}></motion.div>
       <div className="bgWhite">
+        <Parallax />
         <Logo />
         <Card />
       </div>

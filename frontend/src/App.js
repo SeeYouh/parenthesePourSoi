f74398c66b37d739fd3b33058f940c61ed394d00
Julien Froger <webdev.julienfroger@gmail.com>
2023-07-06 17:31:49 +0200

    changement(App.js) : ajouter la prise en charge de la bibliothèque react-locomotive-scroll pour le défilement en douceur changement(_productDetails.scss) : commenter les styles pour .bgProductDetails et .bg-img-blur-desaturate changement(_productCard.scss) : ajouter des styles pour .bgProductDetails, y compris la largeur et la hauteur à 100% et une bordure inférieure droite arrondie avec un pseudo-élément avant

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 import Home from "./pages/Home";
 import { RadioProvider } from "./components/utils/radioContext";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
 
 const App = () => {
+  const containerRef = useRef(null);
+
   return (
-    <RadioProvider>
-      <div className="home">
-        <Home />
-      </div>
-    </RadioProvider>
+    <LocomotiveScrollProvider
+      options={{
+        smooth: true,
+        // lerp: 0.1,
+        // multiplier: 1.5,
+      }}
+      watch={[]}
+      containerRef={containerRef}
+    >
+      <RadioProvider>
+        <div className="home">
+          <Home />
+        </div>
+      </RadioProvider>
+    </LocomotiveScrollProvider>
   );
 };
 

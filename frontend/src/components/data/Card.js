def24a475b6d1ea26d1df3f9baa1a12617f44c56
Julien Froger <webdev.julienfroger@gmail.com>
2023-07-06 08:46:41 +0200

    supprimer(Card.js) : supprimer l'importation et le rendu de FirstCategoryNavBar car il n'est plus utilisé

--- frontend/src/components/data/Card.js
+++ frontend/src/components/data/Card.js
@@ -1,12 +1,10 @@
 import React from "react";
-// import FirstCategoryNavBar from "./FirstCategoryNavBar";
 import SecondCategoryNavBar from "./SecondCategoryNavBar";
 import ProductCardList from "./ProductCardList";
 
 const Card = () => {
   return (
     <main>
-      {/* <FirstCategoryNavBar /> */}
       <div>
         <SecondCategoryNavBar />
         <ProductCardList />

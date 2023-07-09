ef4a2c49097169e8a7863d5996e612620931dcb1
Julien Froger <webdev.julienfroger@gmail.com>
2023-07-09 18:33:25 +0200

    corriger(ProductCardList.js) : supprimer le code commenté et inutilisé corriger(productList.js) : supprimer le commentaire de test

--- frontend/src/components/data/ProductCardList.js
+++ frontend/src/components/data/ProductCardList.js
@@ -1,99 +1,3 @@
-// import React, { useContext, useEffect, useState } from "react";
-// import { motion } from "framer-motion";
-// import ProductDetails from "../ProductDetails";
-// import { productList } from "../../data/productList";
-// import ProductCard from "./ProductCard";
-// import { RadioContext } from "../utils/radioContext";
-// import { scroller } from "react-scroll";
-
-// const ProductCardList = () => {
-//   const {
-//     selectedProduct,
-//     setSelectedProduct,
-//     isActive,
-//     setIsActive,
-//     selectedRadio,
-//     setSelectedRadio,
-//     selectedSubRadio,
-//     setSelectedSubRadio,
-//   } = useContext(RadioContext);
-
-//   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-//   const handleProductClick = (product) => {
-//     setSelectedProduct(product);
-//     setSelectedRadio(product.firstCategory[0]);
-//     setSelectedSubRadio(product.secondCategory[0]);
-//     setIsActive(true);
-//     scroller.scrollTo("productList", {
-//       smooth: "easeInOutQuint",
-//       duration: 1000,
-//       offset: -125,
-//     });
-//   };
-
-//   useEffect(() => {
-//     const handleResizeWidth = () => setWindowWidth(window.innerWidth);
-//     window.addEventListener("resize", handleResizeWidth);
-//     return () => window.removeEventListener("resize", handleResizeWidth);
-//   }, []);
-
-//   return (
-//     <motion.div
-//       initial={{ x: 0 }}
-//       animate={{
-//         x:
-//           isActive && windowWidth >= 1073 && windowWidth < 1339
-//             ? 300
-//             : isActive && windowWidth >= 1340 && windowWidth < 1712
-//             ? 150
-//             : 0,
-//       }}
-//       className="arrayProductCardContainer"
-//     >
-//       <motion.div
-//         initial={{ width: "100%" }}
-//         animate={{
-//           width:
-//             isActive && windowWidth <= 1072
-//               ? "100%"
-//               : isActive && windowWidth >= 1073 && windowWidth <= 1339
-//               ? "70%"
-//               : isActive && windowWidth >= 1340 && windowWidth < 1600
-//               ? "80%"
-//               : "100%",
-//         }}
-//         className="arrayProductCard"
-//       >
-//         {selectedProduct ? (
-//           <ProductDetails product={selectedProduct} />
-//         ) : (
-//           productList
-//             .filter((product) =>
-//               selectedRadio
-//                 ? product.firstCategory.includes(selectedRadio)
-//                 : true
-//             )
-//             .filter((product) =>
-//               selectedSubRadio
-//                 ? product.secondCategory.includes(selectedSubRadio)
-//                 : true
-//             )
-//             .sort((a, b) => a.nameProduct.localeCompare(b.nameProduct))
-//             .map((productDetails, index) => (
-//               <ProductCard
-//                 key={index}
-//                 product={productDetails}
-//                 onProductClick={handleProductClick}
-//               />
-//             ))
-//         )}
-//       </motion.div>
-//     </motion.div>
-//   );
-// };
-
-// export default ProductCardList;
 import React, { useContext, useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import ProductDetails from "../ProductDetails";
@@ -195,7 +99,7 @@ const ProductCardList = () => {
       >
         {selectedProduct ? <ProductDetails product={selectedProduct} /> : null}
       </motion.div>
-    </div>
+  </motion.div>
   );
 };
 

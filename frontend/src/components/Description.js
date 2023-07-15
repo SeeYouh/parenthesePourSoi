2400ab05cf6ab217997dfbc1d4c8738dd8d0b272
Julien Froger <webdev.julienfroger@gmail.com>
2023-07-15 14:24:24 +0200

    corriger(Description.js) : ajouter une nouvelle variable productParagraph pour stocker productDescription.product.summary.paragraph corriger(Description.js) : déplacer le code pour afficher productDescription.subTitleLove et productDescription.textLove à l'intérieur de la boucle corriger(Description.js) : ajouter une nouvelle section "youWillLikeIt" pour afficher les produits connexes avec une image et un texte fonctionnalité(productList.js) : ajouter des images pour les produits connexes corriger(_productCard.scss) : ajouter du style pour la section "youWillLikeIt" et les images des produits connexes

--- frontend/src/components/Description.js
+++ frontend/src/components/Description.js
@@ -2,80 +2,84 @@ import React from "react";
 
 const Description = ({ productDescription }) => {
   const themeColors = productDescription.product.colors;
+  const productParagraph = productDescription.product.summary.paragraph;
+
   return (
     <>
       <div className="bgParagraph">
-        {productDescription.product.summary.paragraph.map(
-          (productDescription, index) => (
-            <div
-              key={index}
-              className={
-                index === 1
-                  ? "productDescriptPresentFirst"
-                  : "productDescriptPresentNext"
-              }
-            >
-              {index === 1 && (
-                <div>
-                  <h3
-                    style={{
-                      color: themeColors.textColor
-                    }}
-                  >
-                    {" "}
-                    {productDescription.subTitle}{" "}
-                  </h3>
-                  <p>{productDescription.text}</p>
-                </div>
-              )}
-              {index === 1 && (
-                <picture>
-                  <source
-                    media="(max-width: 1439px)"
-                    srcSet={productDescription.picture450}
-                  />
-                  <source
-                    media="(min-width: 1440px) and (max-width: 1600px)"
-                    srcSet={productDescription.picture675}
-                  />
-                  <source
-                    media="(min-width: 1601px)"
-                    srcSet={productDescription.picture900}
-                  />
-                  <img
-                    src={productDescription.picture900}
-                    alt={productDescription.alt}
-                  />
-                </picture>
-              )}
-              {index !== 1 && (
-                <>
-                  <h3
-                    style={{
-                      color: themeColors.textColor
-                    }}
-                  >
-                    {" "}
-                    {productDescription.subTitle}{" "}
-                  </h3>
-                  <p>{productDescription.text}</p>
-                </>
-              )}
+        {productParagraph.map((productDescription, index) => (
+          <div
+            key={index}
+            className={
+              index === 1
+                ? "productDescriptPresentFirst"
+                : "productDescriptPresentNext"
+            }
+          >
+            {index === 1 && (
               <div>
                 <h3
                   style={{
                     color: themeColors.textColor
                   }}
                 >
-                  {productDescription.subTitleLove}
+                  {" "}
+                  {productDescription.subTitle}{" "}
                 </h3>
-                <div className="youWillLikeIt">
-                  <p>{productDescription.textLove}</p>
-                </div>
+                <p>{productDescription.text}</p>
+              </div>
+            )}
+            {index === 1 && (
+              <picture>
+                <source
+                  media="(max-width: 1439px)"
+                  srcSet={productDescription.picture450}
+                />
+                <source
+                  media="(min-width: 1440px) and (max-width: 1600px)"
+                  srcSet={productDescription.picture675}
+                />
+                <source
+                  media="(min-width: 1601px)"
+                  srcSet={productDescription.picture900}
+                />
+                <img
+                  src={productDescription.picture900}
+                  alt={productDescription.alt}
+                />
+              </picture>
+            )}
+            {index !== 1 && (
+              <>
+                <h3
+                  style={{
+                    color: themeColors.textColor
+                  }}
+                >
+                  {" "}
+                  {productDescription.subTitle}{" "}
+                </h3>
+                <p>{productDescription.text}</p>
+              </>
+            )}
+            <h3
+              style={{
+                color: themeColors.textColor
+              }}
+            >
+              {productDescription.subTitleLove}
+            </h3>
+            <div className="youWillLikeIt">
+              <div className="youWillLikeIt-content">
+                <img
+                  src={productDescription.pictureLove}
+                  alt={productDescription.alt}
+                />
+                <p>{productDescription.textLove}</p>
               </div>
             </div>
-          )
-        )}
+          </div>
+        ))}
       </div>
       ;
     </>

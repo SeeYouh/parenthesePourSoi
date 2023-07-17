3832ee38a90881492bf4fd3fb935df7599714e00
Julien Froger <webdev.julienfroger@gmail.com>
2023-07-17 21:45:19 +0200

    ajouter les images bienEtreInterieurIllusText450.jpg, bienEtreInterieurIllusText675.jpg et bienEtreInterieurIllusText900.jpg dans le dossier d'images du frontend modifier le fichier bienEtreInterieur.js pour ajouter des sous-titres et des images pour chaque paragraphe ajouter une section "Vous aimerez aussi..." avec une image et un texte ajouter une propriété "colors" avec la couleur du texte "#8a7540" pour le produit bienEtreInterieur

--- frontend/src/data/product/bienEtreInterieur.js
+++ frontend/src/data/product/bienEtreInterieur.js
@@ -6,22 +6,46 @@ export const bienEtreInterieur = {
     title: "Équilibrez votre corps, équilibrez votre vie.",
     paragraph: [
       {
-        text: "Notre produit agit en régulant votre microbiote, favorisant ainsi un équilibre optimal. Le microbiote est essentiel pour votre bien-être, car il participe à de nombreuses fonctions, de la digestion à l'absorption des nutriments, en passant par le renforcement de votre système immunitaire. "
+        subTitle: "Équilibre du microbiote",
+        text: "Notre produit agit en régulant votre microbiote, favorisant ainsi un équilibre optimal. Le microbiote est essentiel pour votre bien-être, car il participe à de nombreuses fonctions, de la digestion à l'absorption des nutriments, en passant par le renforcement de votre système immunitaire. En rétablissant l'équilibre de votre microbiote, les prébiotiques et probiotiques contribuent à améliorer votre santé globale et à prévenir divers troubles digestifs et maladies chroniques associées."
       },
       {
-        text: "En rétablissant l'équilibre de votre microbiote, notre produit contribue à améliorer votre santé globale et à prévenir divers troubles digestifs et maladies chroniques associées. Le pouvoir antioxydant de notre produit aide à neutraliser les toxines présentes dans votre organisme. Il contribue également à diminuer les troubles digestifs en maintenant l'équilibre des bonnes bactéries et en améliorant la flore intestinale. Recommandé pour les maladies chroniques telles que le côlon irritable et la maladie de Crohn (avec avis médical), il améliore également votre santé globale."
+        subTitle: "Pouvoir antioxydant",
+        text: "Le pouvoir antioxydant de notre produit aide à neutraliser les toxines présentes dans votre organisme. Il contribue également à diminuer les troubles digestifs en maintenant l'équilibre des bonnes bactéries et en améliorant la flore intestinale. Recommandé pour les maladies chroniques telles que le côlon irritable et la maladie de Crohn (avec avis médical), il améliore également votre santé globale.",
+        picture450: "./img/jpg/product/bienEtreInterieurIllusText450.jpg",
+        picture675: "./img/jpg/product/bienEtreInterieurIllusText675.jpg",
+        picture900: "./img/jpg/product/bienEtreInterieurIllusText900.jpg"
       },
       {
-        text: "Notre produit de bien-être intérieur joue un rôle clé dans la gestion du poids en contrôlant la satiété et en réduisant les envies de sucre et de gras. En favorisant un microbiote équilibré, il permet de vous aider à atteindre et maintenir un poids santé. De plus, il booste votre système immunitaire, facilite l'absorption des nutriments et régule le taux de cholestérol."
+        subTitle: "Gestion du poids",
+        text: "Notre solution de bien-être intérieur joue un rôle clé dans la gestion du poids en contrôlant la satiété et en réduisant les envies de sucre et de gras. En favorisant un microbiote équilibré, il permet de vous aider à atteindre et maintenir un poids santé. De plus, il booste votre système immunitaire, facilite l'absorption des nutriments et régule le taux de cholestérol."
       },
       {
-        text: "Notre produit vous offre un bien-être complet en boostant l'énergie naturelle de votre corps grâce à la synthèse des vitamines B et K. Il contribue également à réduire le stress et l'anxiété, à combattre la fatigue, à améliorer la qualité du sommeil et à stimuler votre humeur, vous permettant ainsi de profiter pleinement de chaque journée."
+        subTitle: "Boost d'énergie et bien-être",
+        text: "Il vous offre un bien-être complet en boostant l'énergie naturelle de votre corps grâce à la synthèse des vitamines B et K. Il contribue également à réduire le stress et l'anxiété, à combattre la fatigue, à améliorer la qualité du sommeil et à stimuler votre humeur, vous permettant ainsi de profiter pleinement de chaque journée."
       },
       {
-        text: "Notre produit offre des bienfaits spécifiques pour les femmes en diminuant les troubles vaginaux et en améliorant le tractus génital. Il vous permet de prendre soin de votre santé intime de manière naturelle et équilibrée."
+        subTitle: "Bien-être féminin",
+        text: "Le bien-être intérieur n’est pas seulement digestif pour vous Mesdames, car ce produit offre des bienfaits spécifiques pour les femmes en diminuant les troubles vaginaux et en améliorant le tractus génital. Il vous permet de prendre soin de votre santé intime de manière naturelle et équilibrée."
       },
       {
-        text: "Expérimentez ce produit, c’est côtoyez un bien-être intérieur inégalé !"
+        subTitle: " Le Bien-être Réinventé",
+        text: "Expérimentez ces pré-probiotiques, c’est côtoyez un bien-être intérieur inégalé !"
+      },
+      {
+        subTitle: "Vous aimerez aussi…",
+        text: ""
+      },
+      {
+        subTitle: "",
+        text: ""
+      }
+    ],
+    youLikeIt: [
+      {
+        pictureLove: "./img/jpg/product/goutteMinceur.jpg",
+        textLove:
+          "Ce produit peut être associé à nos gouttes super puissantes dans le cadre d’une perte de poids saine et durable."
       }
     ]
   },
@@ -30,5 +54,8 @@ export const bienEtreInterieur = {
     getSecondCategoryById("1Sante1TroublesDigestifsIntestinauxRespiratoires")
       .text
   ],
-  picture: "./img/jpg/product/bienEtreInterieur.jpg"
+  picture: "./img/jpg/product/bienEtreInterieur.jpg",
+  colors: {
+    textColor: "#8a7540"
+  }
 };

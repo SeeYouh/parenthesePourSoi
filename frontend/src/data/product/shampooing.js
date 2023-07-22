a6ef6da1df1c120daef736f5bb48a64f262e5d42
Julien Froger <webdev.julienfroger@gmail.com>
2023-07-22 02:59:57 +0200

    add and update file product

--- frontend/src/data/product/shampooing.js
+++ frontend/src/data/product/shampooing.js
@@ -6,27 +6,37 @@ export const shampooing = {
     title: "Sublimez vos cheveux avec ce shampooing d’exception.",
     paragraph: [
       {
-        subTitles: "",
-        text: "Notre shampooing offre à vos cheveux le traitement qu'ils méritent avec notre produit révolutionnaire. Il nettoie en profondeur, éliminant les impuretés et les résidus de produits capillaires, pour des cheveux propres et sains. De plus, sa formule spéciale revitalise et hydrate intensément chaque mèche, rendant vos cheveux doux, brillants et pleins de vitalité."
+        subTitle:
+          "Offrez une nouvelle vie à vos cheveux avec notre shampooing révolutionnaire",
+        text: "Votre chevelure est précieuse et mérite le plus grand soin. Notre shampooing révolutionnaire est la solution que vous attendiez. Il nettoie vos cheveux en profondeur, en éliminant efficacement toutes les impuretés et les résidus indésirables de produits capillaires. Imaginez une chevelure parfaitement propre et incroyablement saine. Ce rêve peut devenir réalité grâce à notre formule innovante."
       },
       {
-        subTitles: "",
-        text: "Notre produit ne se limite pas à cela, il est également votre allié pour un cuir chevelu sain et protégé. Sa formule contribue à apaiser les problèmes cutanés tels que les irritations et les rougeurs. De plus, il combat efficacement les pellicules, réduit les démangeaisons et  l'eczéma capillaire restaurant l'équilibre du cuir chevelu. ",
+        subTitle:
+          "Hydratation et revitalisation intense : une transformation visible",
+        text: "Notre shampooing fait bien plus que nettoyer vos cheveux. Il leur offre une cure de jouvence grâce à sa formule riche en ingrédients naturels hydratants. Chaque mèche de vos cheveux bénéficie d'un bain d'hydratation, revitalisant en profondeur les fibres capillaires.Cette hydratation intense a le pouvoir de redonner vie aux cheveux secs et abîmés. Les cheveux sont nourris de l'intérieur, devenant plus souples, plus résistants et pleins de vitalité. ",
         picture450: "./img/jpg/product/shampooingIllusText450.jpg",
         picture675: "./img/jpg/product/shampooingIllusText675.jpg",
         picture900: "./img/jpg/product/shampooingIllusText900.jpg"
       },
       {
-        subTitles: "",
-        text: "Notre shampooing convient aux cheveux colorés."
+        subTitle: "Une peau apaisée et protégée pour un confort inégalé",
+        text: "Notre shampooing est également un allié puissant pour votre cuir chevelu. Sa composition douce, débarrassée de tout produit chimique agressif, respecte votre peau, l'apaise et la protège. Les problèmes de cuir chevelu tels que les irritations, les rougeurs, les démangeaisons et même l'eczéma sont atténués, vous offrant un confort quotidien inégalé. De plus, sa capacité à lutter contre les pellicules contribue à rétablir l'équilibre naturel de votre cuir chevelu, pour une sensation de fraîcheur et de propreté tout au long de la journée."
       },
       {
-        subTitles: "",
-        text: "Offrez-vous une expérience luxueuse en découvrant ce shampooing qui prend soin de vos cheveux, ravive votre peau et élimine les problèmes courants du cuir chevelu."
+        subTitle: "Préservez la beauté de vos cheveux colorés",
+        text: "Ce shampooing n'est pas seulement un nettoyant efficace, il respecte également la beauté de vos cheveux colorés. Sa formule délicate nettoie en douceur tout en préservant l'éclat et l'intensité de votre coloration. Ainsi, vos cheveux colorés gardent leur beauté et leur luminosité plus longtemps."
       },
       {
-        subTitles: "",
-        text: "Pour compléter votre expérience de bien-être, je vous recommande vivement de jeter un coup d'œil à notre après-shampooing, qui se marie parfaitement avec notre shampooing."
+        subTitle: "Plongez dans l'expérience luxueuse de notre shampooing",
+        text: "Prenez soin de vos cheveux comme jamais auparavant et offrez-vous une expérience capillaire digne d'un salon de coiffure à domicile. Notre shampooing transforme chaque lavage de cheveux en un moment de plaisir pur, nourrissant, protégeant et sublimant votre chevelure. Laissez-vous tenter et ressentez la différence : des cheveux plus sains, une couleur préservée, un cuir chevelu apaisé et une sensation de bien-être incroyable. Essayez-le dès aujourd'hui et découvrez le secret d'une chevelure sublime et éclatante de santé."
+      },
+      {
+        subTitle: "Vous aimerez aussi…",
+        text: ""
+      },
+      {
+        subTitle: "",
+        text: ""
       }
     ],
     youLikeIt: [

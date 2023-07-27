c50597309c1c02e4f17bbaef3e5323385b42bf6f
Julien Froger <webdev.julienfroger@gmail.com>
2023-07-27 20:17:26 +0200

    corriger(HERelaxante.js) : ajouter des identifiants uniques pour chaque paragraphe fonctionnalité(HERelaxante.js) : ajouter des identifiants uniques pour chaque paragraphe afin de faciliter la navigation et la référence corriger(boosterDenergie.js) : ajouter des identifiants uniques pour chaque paragraphe fonctionnalité(boosterDenergie.js) : ajouter des identifiants uniques pour chaque paragraphe afin de faciliter la navigation et la référence corriger(gelDouche.js) : ajouter des identifiants uniques pour chaque paragraphe fonctionnalité(gelDouche.js) : ajouter des identifiants uniques pour chaque paragraphe afin de faciliter la navigation et la référence corriger(osMuscle.js) : ajouter des identifiants uniques pour chaque paragraphe fonctionnalité(osMuscle.js) : ajouter des identifiants uniques pour chaque paragraphe afin de faciliter la navigation et la référence

--- frontend/src/data/product/boosterDenergie.js
+++ frontend/src/data/product/boosterDenergie.js
@@ -6,10 +6,12 @@ export const boosterEnergie = {
     title: "Un mélange vivifiant pour booster votre journée.",
     paragraph: [
       {
+        id: "0boosterEnergie",
         subTitle: "Boostez votre énergie",
         text: "Votre santé est notre priorité. Notre booster d’énergie stimule votre système immunitaire et cardiovasculaire, renforçant ainsi votre résistance aux maladies et protégeant votre cœur. Les oméga 6 et 9 présents dans notre formule améliorent l'attention et la vigilance mentale, vous permettant de rester concentré(e) et productif(ve) tout au long de la journée."
       },
       {
+        id: "1boosterEnergie",
         subTitle: "Retrouvez votre bien-être",
         text: [
           "Notre booster d'énergie est bien plus qu'un simple complément, c'est le véritable catalyseur de votre bien-être. Il dynamise votre corps en stimulant votre énergie naturelle, vous offrant une vitalité renouvelée tout au long de la journée. Grâce à sa formule unique, il combat efficacement la fatigue et vous aide à retrouver un sommeil réparateur, vous permettant de vous réveiller frais et revitalisé chaque matin.",
@@ -21,57 +23,63 @@ export const boosterEnergie = {
         picture900: "./img/jpg/product/BEIllusText900.jpg"
       },
       {
+        id: "2boosterEnergie",
         subTitle: "Gérez votre poids",
         text: "Notre produit est également une solution efficace pour vous aider à atteindre vos objectifs de gestion du poids car il augmente de 17% la combustion des graisses."
       },
       {
+        id: "3boosterEnergie",
         subTitle: "Nourrissez votre peau",
         text: "Grâce à ses propriétés antioxydantes exceptionnelles, notre produit réduit visiblement les signes de l'âge, vous offrant une peau éclatante et rajeunie. En nourrissant votre peau de manière profonde, il révèle votre beauté naturelle, vous permettant de vous sentir confiant(e) et épanoui(e) dans votre propre peau."
       },
       {
+        id: "4boosterEnergie",
         subTitle: "Pour vous, Mesdames",
         text: "Mesdames, notre booster d’énergie a aussi des effets sur les désagréments féminins car il diminue efficacement les douleurs menstruelles, soulageant ainsi les gênes liées à votre cycle. De plus, il prévient les infections urinaires, vous offrant une protection essentielle pour votre santé intime."
       },
       {
+        id: "5boosterEnergie",
         subTitle: "Choisissez-le pour tout ces bienfaits",
         text: "Faites le choix d’un bien-être optimal et votre corps vous remerciera pour cette décision qui va changer votre quotidien !"
       },
       {
+        id: "6boosterEnergie",
         subTitle: "Vous aimerez aussi…",
         text: ""
       },
       {
+        id: "7boosterEnergie",
         subTitle: "",
         text: ""
       }
     ],
     youLikeIt: [
       {
-        id: "bEGoutteMinceur0",
+        id: "BdEGoutteMinceur0",
         pictureLove: "./img/jpg/product/goutteMinceur.jpg",
         textLove:
           "Pour une cure minceur, ce booster d’énergie fonctionne en synergie parfaite avec nos gouttes minceur naturelles, conçues pour brûler efficacement les graisses sous-cutanées et viscérales, tout en bloquant le stockage des graisses indésirables."
       },
       {
-        id: "bEHarmonie1",
+        id: "BdEHarmonie1",
         pictureLove: "./img/jpg/product/harmonie.jpg",
         textLove:
           "Pour un bien-être psychologique et la prévention du burn-out, ce produit s'associe idéalement à un autre de nos produits. Une combinaison parfaite pour retrouver l'équilibre intérieur et une sérénité épanouissante."
       },
       {
-        id: "bERegenerateurCellulaire2",
+        id: "BdERegenerateurCellulaire2",
         pictureLove: "./img/jpg/product/regenerateurCellulaire.jpg",
         textLove:
           "Pour les problèmes d’ordre cutané, notre produit chouchou régénérateur cellulaire est un véritable miracle pour votre peau. Une expérience naturelle et revitalisante pour une peau éclatante et rayonnante."
       },
       {
-        id: "beOsMuscle3",
+        id: "BdEOsMuscle3",
         pictureLove: "./img/jpg/product/osMuscle.jpg",
         textLove:
           "Vous vous sentez stressée ? Anxieuse ? Un apport en magnésium est essentiel pour contrer les effets néfastes du stress sur votre corps. Découvrez une expérience apaisante pour retrouver équilibre et bien-être."
       },
       {
-        id: "beEquilibreCorpsEsprit4",
+        id: "BdEEquilibreCorpsEsprit4",
         pictureLove: "./img/jpg/product/equilibreCorpsEsprit.jpg",
         textLove:
           "Parfois, la fatigue est associée à un manque de sommeil ou un repos non récupérateur. Notre produit naturel est spécialement conçu pour vous offrir un sommeil profond et réparateur. Plongez dans des nuits paisibles et revitalisantes qui vous donneront l'énergie et le bien-être nécessaires pour une journée épanouissante."
--- frontend/src/data/product/gelDouche.js
+++ frontend/src/data/product/gelDouche.js
@@ -6,10 +6,12 @@ export const gelDouche = {
     title: "Une expérience de soin incomparable pour votre peau.",
     paragraph: [
       {
+        id: "0gelDouche",
         subTitle: "Un Nettoyage Profond et Respectueux",
         text: "Doté d'une formule délicate, ce produit offre un nettoyage en profondeur sans compromettre l'intégrité de votre peau. Il élimine efficacement les impuretés, la saleté et les résidus chimiques indésirables qui peuvent s'accumuler au fil de la journée. Après chaque utilisation, votre peau retrouve toute sa fraîcheur et sa pureté, libérée de toute substance nocive. C'est un véritable soin de propreté qui respecte l'équilibre naturel de votre épiderme."
       },
       {
+        id: "1gelDouche",
         subTitle: "Purification et Éclat pour une Beauté Naturelle",
         text: "En plus de son action nettoyante, cette formule agit comme un véritable purificateur. Elle élimine les toxines superficielles qui peuvent ternir l'éclat de votre teint et obstruer vos pores. Avec chaque utilisation, votre peau respire mieux, vous ressentez une sensation de propreté et de légèreté. Votre peau est naturellement belle, éclatante et pleine de vitalité.",
         picture450: "./img/jpg/product/gelDoucheIllusText450.jpg",
@@ -17,31 +19,35 @@ export const gelDouche = {
         picture900: "./img/jpg/product/gelDoucheIllusText900.jpg"
       },
       {
+        id: "2gelDouche",
         subTitle: "Soulagement et Confort pour les Peaux Sensibles",
         text: "Grâce à sa composition douce et naturelle, ce soin est idéal pour les peaux sensibles et réactives. Il réduit les rougeurs, les démangeaisons et les irritations, apportant un soulagement bienvenu et un confort optimal à votre peau. Il respecte la sensibilité de votre peau tout en offrant un nettoyage efficace."
       },
       {
+        id: "3gelDouche",
         subTitle: "Apaisement des Affections Cutanées",
         text: "Ce produit est également bénéfique pour ceux qui souffrent d'affections cutanées comme l'eczéma et le psoriasis. Grâce à ses propriétés apaisantes, il contribue à calmer ces conditions, offrant une solution de soin de la peau douce et efficace. Les personnes aux peaux sensibles et sujettes à ces affections cutanées seront ravies de constater les bienfaits de ce soin, qui apporte un apaisement notable à chaque utilisation."
       },
       {
+        id: "4gelDouche",
         subTitle: "Vous aimerez aussi…",
         text: ""
       },
       {
+        id: "5gelDouche",
         subTitle: "",
         text: ""
       }
     ],
     youLikeIt: [
       {
-        id: "gDcocoOil0",
+        id: "GDcocoOil0",
         pictureLove: "./img/jpg/product/cocoOil.jpg",
         textLove:
           "Le gel douche combiné à notre huile hydratante permet à votre peau de retrouver confort et hydratation."
       },
       {
-        id: "gDregenerateurCellulaire1",
+        id: "GDregenerateurCellulaire1",
         pictureLove: "./img/jpg/product/regenerateurCellulaire.jpg",
         textLove:
           "Concernant les problèmes de peau, le gel douche s’associe très souvent à notre chouchou qui régénère les cellules. Ce combo est idéal pour les soucis cutanés."

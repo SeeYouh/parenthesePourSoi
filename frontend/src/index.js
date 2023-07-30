eb74ba9eb3dfa68be6971c0497bcabe4537a3285
Julien Froger <webdev.julienfroger@gmail.com>
2023-07-31 01:00:39 +0200

    chore(package.json): mettre à jour la dépendance "react-router-dom" vers la version 6.14.2 style(sass): ajouter une propriété "cursor: pointer" aux boutons pour indiquer qu'ils sont cliquables

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,23 @@
-import "./sass/main.scss";
+import './sass/main.scss';
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react';
 
-import App from "./App";
-import PagesError from "./pages/PagesError";
-import React from "react";
-import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+import PagesError from './pages/PagesError';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
+      <Route path=":id" element={<App />} />
       <Route path="*" element={<PagesError />} />
     </Routes>
   </BrowserRouter>

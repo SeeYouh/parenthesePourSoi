fc4328f43cc9b42b68135b203e3e68faa8460e76
Julien Froger <webdev.julienfroger@gmail.com>
2023-07-31 02:49:39 +0200

    corriger(index.js) : changer la casse des importations de fichiers sass et react-router-dom fonctionnalité(index.js) : ajouter une route pour afficher une liste de cartes de produits en fonction de la catégorie sélectionnée

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,20 @@
-import './sass/main.scss';
+import "./sass/main.scss";
 
-import React from 'react';
+import React from "react";
 
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-} from 'react-router-dom';
-import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
-import PagesError from './pages/PagesError';
+import App from "./App";
+import PagesError from "./pages/PagesError";
+import ProductCardList from "./components/data/ProductCardList";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path=":id" element={<App />} />
+      <Route path="/category/:id" element={<ProductCardList />} />
       <Route path="*" element={<PagesError />} />
     </Routes>
   </BrowserRouter>

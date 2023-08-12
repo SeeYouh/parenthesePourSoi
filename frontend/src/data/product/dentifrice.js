5ece339aa916e485334ff6c2a37018ff854add66
Julien Froger <webdev.julienfroger@gmail.com>
2023-08-12 11:38:08 +0200

    changement(images) : ajouter de nouvelles images de produits De nouvelles images de produits ont été ajoutées dans le répertoire "frontend/public/img/jpg/product". Les nouvelles images sont : "cremeNightIllusText.jpg", "cremeNightIllusText450.jpg", "cremeNightIllusText675.jpg", "cremeNightIllusText900.jpg", "eauToniqueIllusText.jpg", "eauToniqueIllusText450.jpg", "eauToniqueIllusText675.jpg" et "eauToniqueIllusText900.jpg". Ces images sont nécessaires pour afficher les illustrations des produits correspondants sur le site web.
    corriger(frontend/src/data/product/dentifrice.js) : mettre à jour les sous-titres et les textes du dentifrice
    fonctionnalité(frontend/src/data/product/dentifrice.js) : améliorer la description du dentifrice et ajouter des informations sur ses bienfaits
    
    Les sous-titres et les textes du dentifrice ont été modifiés pour améliorer la clarté et la cohérence du contenu. Les sous-titres ont été reformulés pour mieux représenter les avantages du dentifrice, et les textes ont été réorganisés pour fournir des informations plus complètes sur les bienfaits du produit. De plus, des informations supplémentaires ont été ajoutées sur l'huile essentielle de menthe poivrée, l'absence de produits nocifs et la sensation de fraîcheur durable après chaque brossage. Enfin, des recommandations de produits complémentaires ont été ajoutées pour offrir une expérience de soin complète.
    
    corriger(gelDouche.js) : modifier les sous-titres et les textes pour améliorer la clarté et la cohérence
    fonctionnalité(gelDouche.js) : améliorer la description du produit de gel douche pour mieux mettre en valeur ses avantages et ses bienfaits pour la peau. Ajouter des informations sur les produits complémentaires qui peuvent être utilisés avec le gel douche.
    Les sous-titres et les textes ont été modifiés pour mieux décrire les avantages du gel douche. Les nouvelles descriptions mettent en évidence les propriétés nettoyantes, purifiantes et apaisantes du produit, ainsi que ses bienfaits pour les peaux sensibles et les affections cutanées. De plus, des informations sur les produits complémentaires ont été ajoutées pour offrir une expérience de soin plus complète aux clients.

--- frontend/src/data/product/dentifrice.js
+++ frontend/src/data/product/dentifrice.js
@@ -9,39 +9,33 @@ export const dentifrice = {
     paragraph: [
       {
         id: "0Dentifrice",
-        subTitle: "Ingrédients naturels",
-        text: "Notre dentifrice est formulé à partir d'ingrédients naturels de haute qualité. Vous n'y trouverez aucun produit chimique agressif. Il offre un blanchiment naturel en éliminant les tâches en douceur et vous aide à obtenir des dents plus éclatantes qui illumine votre sourire. "
+        subTitle: "Choisissez votre santé",
+        text: "Notre dentifrice est formulé à partir d'ingrédients naturels de haute qualité. Vous n'y trouverez aucun produit chimique agressif. Il offre un blanchiment naturel en éliminant les tâches en douceur et vous aide à obtenir des dents plus éclatantes qui illumine votre sourire."
       },
       {
         id: "1Dentifrice",
-        subTitle: "Gencives saines",
-        text: "Il est enrichi en extraits de plantes sélectionnés pour leurs propriétés apaisantes et antibactériennes. Ces ingrédients naturels travaillent en synergie pour aider à maintenir vos gencives en bonne santé et revitalisées. Ils combattent les bactéries qui peuvent causer des problèmes de gencives et apaisent les inflammations, favorisant ainsi une bouche saine et un sourire éclatant. Que vous cherchiez à prévenir les problèmes de gencives ou à soutenir une santé buccale déjà optimale, notre produit offre une solution naturelle et efficace.",
+        subTitle: "Adoptez ses bienfaits",
+        text: [
+          "Il est enrichi en extraits de plantes sélectionnés pour leurs propriétés apaisantes et antibactériennes. Ces ingrédients naturels travaillent en synergie pour aider à maintenir vos gencives en bonne santé et revitalisées. Ils combattent les bactéries qui peuvent causer des problèmes de gencives et apaisent les inflammations, favorisant ainsi une bouche saine et un sourire éclatant. Que vous cherchiez à prévenir les problèmes de gencives ou à soutenir une santé buccale déjà optimale, notre produit offre une solution naturelle et efficace.",
+          "Avec l'huile essentielle de menthe poivrée, notre produit laisse une agréable sensation de fraîcheur dans votre bouche. Dites adieu à la mauvaise haleine et profitez d'une bouffée de fraîcheur durable après chaque brossage.",
+          "Notre dentifrice est sans fluorure pour ceux qui préfèrent éviter cet élément, sans triclosan qui peut être nocif, sans paraben pour une tranquillité d'esprit, sans édulcorants artificiels pour une saveur pure, et sans colorants pour une expérience naturelle authentique."
+        ],
         picture675: "/img/jpg/product/dentifriceIllusText675.jpg",
         picture450: "/img/jpg/product/dentifriceIllusText450.jpg",
         picture900: "/img/jpg/product/dentifriceIllusText900.jpg"
       },
       {
         id: "2Dentifrice",
-        subTitle: "Fraîcheur durable",
-        text: "Avec l'huile essentielle de menthe poivrée, notre produit laisse une agréable sensation de fraîcheur dans votre bouche. Dites adieu à la mauvaise haleine et profitez d'une bouffée de fraîcheur durable après chaque brossage."
-      },
-      {
-        id: "3Dentifrice",
-        subTitle: "Sans produits nocifs",
-        text: "Notre dentifrice est sans fluorure pour ceux qui préfèrent éviter cet élément, sans triclosan qui peut être nocif, sans paraben pour une tranquillité d'esprit, sans édulcorants artificiels pour une saveur pure, et sans colorants pour une expérience naturelle authentique."
-      },
-      {
-        id: "4Dentifrice",
-        subTitle: "Sourire naturel",
+        subTitle: "Souriez naturellement",
         text: "Découvrez une nouvelle façon de prendre soin de votre sourire, naturellement."
       },
       {
-        id: "5Dentifrice",
+        id: "3Dentifrice",
         subTitle: "Vous aimerez aussi…",
         text: ""
       },
       {
-        id: "6Dentifrice",
+        id: "4Dentifrice",
         subTitle: "",
         text: ""
       }
@@ -49,24 +43,38 @@ export const dentifrice = {
     youLikeIt: [
       {
         id: "Dentifrice0",
-        link: "detox",
-        pictureLove: "/img/jpg/product/detox.jpg",
+        link: "dentitionBlanche",
+        pictureLove: "/img/jpg/product/dentitionBlanche.jpg",
         textLove:
-          "La détox’ étant la base de toute cure minceur, nos gouttes super puissantes devraient également vous intéresser pour perdre du poids sans effet yoyo."
+          "Pour aller encore plus loin dans le blanchiment de vos dents, découvrez notre poudre révolutionnaire. Notre blanchisseur élimine en douceur les impuretés et les tâches, vous offrant un sourire éblouissant. Il protège vos gencives et assure une haleine fraîche durable. Sans ingrédients controversés, notre formule respecte votre santé pour une confiance totale toute la journée. Profitez d'un sourire éclatant et de soins bucco-dentaires complets."
       },
       {
         id: "Dentifrice1",
-        link: "bienEtreInterieur",
-        pictureLove: "/img/jpg/product/bienEtreInterieur.jpg",
+        link: "shampooing",
+        pictureLove: "/img/jpg/product/shampooing.jpg",
         textLove:
-          "Si votre système digestif et intestinal sont déséquilibrés, les pré-probiotiques devraient réguler tout ceci."
+          "Pour rester dans la gamme des soins naturels, choisissez notre shampooing d'exception en association pour une expérience de cheveux doux et sains. Ce shampooing améliore les soucis du cuir chevelu tout en protégeant le cheveu mèche par mèche."
       },
       {
         id: "Dentifrice2",
-        link: "gg",
-        pictureLove: "/img/jpg/product/GG.jpg",
+        link: "gelDouche",
+        pictureLove: "/img/jpg/product/gelDouche.jpg",
         textLove:
-          "Une autre approche consiste à rétablir l'équilibre de votre système digestif et intestinal en fournissant des légumes et des vitamines essentiels par le biais d'une détoxification végétale."
+          "Pour une expérience de bien-être enrichissante, choisissez notre gel douche naturel de la même gamme. Ces deux produits complémentaires transformeront votre routine quotidienne en une expérience de soin remarquable pour vos dents et votre peau. "
+      },
+      {
+        id: "Dentifrice3",
+        link: "apportMineraux",
+        pictureLove: "/img/jpg/product/apportMineraux.jpg",
+        textLove:
+          "Notre concentré de minéraux et oligo-éléments est un puissant allié pour vos os et vos dents. Il a de nombreux avantages et bénéfices pour votre corps et votre santé."
+      },
+      {
+        id: "Dentifrice4",
+        link: "baumeLevres",
+        pictureLove: "/img/jpg/product/baumeLevres.jpg",
+        textLove:
+          "Pour un sourire encore plus éclatant, regardez notre baume à lèvres naturel. Sa formule avancée protège vos lèvres des agressions extérieures et prévient les gerçures et la sécheresse. Enrichi d'ingrédients naturels, il répare et renforce vos lèvres, les laissant douces et résistantes. Offrez à votre sourire une oasis de douceur et de protection pour une apparence saine et éclatante."
       }
     ]
   },

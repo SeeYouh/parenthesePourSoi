c1268a111c0a0b4dcd9f015d98522ccf57dd0a48
Julien Froger <webdev.julienfroger@gmail.com>
2023-08-13 10:31:22 +0200

    corriger(Footer.js) : ajouter la fonctionnalité de défilement vers les sections "politique de confidentialité" et "conditions d'utilisation" lorsqu'un utilisateur clique sur les liens correspondants dans le pied de page corriger(index.js) : ajouter les routes pour les pages "Terms" et "Privacy" corriger(terms.js) : ajouter les barres de navigation pour les catégories "FirstCategoryNavBar" et "SecondCategoryNavBar" dans la page "Terms" Les modifications apportées permettent aux utilisateurs de faire défiler la page vers les sections "politique de confidentialité" et "conditions d'utilisation" lorsqu'ils cliquent sur les liens correspondants dans le pied de page. De plus, les routes pour les pages "Terms" et "Privacy" sont ajoutées, et les barres de navigation pour les catégories sont ajoutées dans la page "Terms".

--- frontend/src/components/data/Footer.js
+++ frontend/src/components/data/Footer.js
@@ -1,13 +1,44 @@
 import React from "react";
 
+import { scroller } from "react-scroll";
+import { useNavigate } from "react-router-dom";
+
 const Footer = () => {
+  const navigate = useNavigate();
+
+  const goToPageAndScrollTerms = async (selector) => {
+    const offset = -305;
+    await navigate("/terms");
+    await scroller.scrollTo(selector, {
+      duration: 1000,
+      smooth: true,
+      offset: offset,
+      spy: true
+    });
+  };
+  const goToPageAndScrollPolicy = async (selector) => {
+    const offset = -305;
+    await navigate("/terms");
+    await scroller.scrollTo(selector, {
+      duration: 1000,
+      smooth: true,
+      offset: offset,
+      spy: true
+    });
+  };
+
   return (
     <footer>
       <div>contact</div>
       <div>Facebook</div>
       <div>Instagram</div>
       <div>Tic Toc</div>
-      <div>mention légales</div>
+      <div onClick={() => goToPageAndScrollPolicy("Privacy")}>
+        politique de confidentialité
+      </div>
+      <div onClick={() => goToPageAndScrollTerms("Terms")}>
+        condition d'utilisation
+      </div>
     </footer>
   );
 };
--- frontend/src/pages/terms.js
+++ frontend/src/pages/terms.js
@@ -1,8 +1,13 @@
 import React from "react";
 
+import FirstCategoryNavBar from "../components/data/FirstCategoryNavBar";
+import SecondCategoryNavBar from "../components/data/SecondCategoryNavBar";
+
 const Terms = () => {
   return (
     <div>
+      <FirstCategoryNavBar />
+      <SecondCategoryNavBar />
       <h1>Terms</h1>
     </div>
   );

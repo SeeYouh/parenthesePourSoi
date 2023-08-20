885bd660d9c61b3aab44fb2e3d7bbff24c6f9d62
Julien Froger <webdev.julienfroger@gmail.com>
2023-08-20 18:48:26 +0200

    corriger(Footer.js) : ajouter la variable currentYear pour afficher l'année actuelle dans le footer corriger(Footer.js) : mettre à jour la mise en page du footer pour améliorer la lisibilité et l'apparence corriger(Footer.js) : mettre à jour les icônes des réseaux sociaux pour qu'elles s'affichent correctement corriger(Footer.js) : mettre à jour les liens des icônes des réseaux sociaux pour qu'ils fonctionnent correctement corriger(Footer.js) : ajouter un style pour les informations légales et les liens vers la politique de confidentialité et les conditions d'utilisation corriger(Footer.js) : ajouter un style pour le texte de contact corriger(generalData.js) : mettre à jour le logo Facebook pour qu'il s'affiche correctement corriger(sass/base/_padding.scss) : supprimer la règle de style .legal p pour éviter les doublons corriger(sass/base/_width.scss) : augmenter la largeur de .logoPictureFooter à 290px pour une meilleure apparence corriger(sass/components/_footer.scss) : mettre à jour la couleur de fond du footer et réduire sa hauteur à 350px corriger(sass/components/_footer.scss) : mettre à jour la taille et la position du logoPictureFooter pour une meilleure apparence corriger(sass/components/_footer.scss) : mettre à jour la mise en page du footer pour une meilleure lisibilité et apparence corriger(sass/utils/_variable.scss) : ajouter les sélecteurs .legalArticle p:before et .legalArticle p pour ajouter des styles aux liens des informations légales La mise à jour du footer améliore son apparence et sa lisibilité. Les icônes des réseaux sociaux sont maintenant affichées correctement et les liens fonctionnent correctement. Les informations légales ont également été mises à jour avec des styles appropriés.

--- frontend/src/components/data/Footer.js
+++ frontend/src/components/data/Footer.js
@@ -7,6 +7,8 @@ import { generalDataImg } from "../../data/generalData";
 import { RadioContext } from "../utils/radioContext";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   const {
     setSelectedRadio,
     setSelectedSubRadio,
@@ -45,44 +47,6 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      <div className="socialNetWorkContainer">
-        <div className="socialNetwork">
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg">
-              <defs>
-                <symbol id="instagram-icon" viewBox="0 0 448 512">
-                  <path d={generalDataImg[4].logoInstagram} />
-                </symbol>
-              </defs>
-              <use xlinkHref="#instagram-icon" height="20px" />
-            </svg>
-          </div>
-        </div>
-        <div className="socialNetwork">
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg">
-              <defs>
-                <symbol id="tiktok-icon" viewBox="0 0 512 512">
-                  <path d={generalDataImg[6].logoTikTok} />
-                </symbol>
-              </defs>
-              <use xlinkHref="#tiktok-icon" height="20px" />
-            </svg>
-          </div>
-        </div>
-        <div className="socialNetwork">
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg">
-              <defs>
-                <symbol id="facebook-icon" viewBox="0 0 448 512">
-                  <path d={generalDataImg[5].logoFacebook} />
-                </symbol>
-              </defs>
-              <use xlinkHref="#facebook-icon" height="20px" />
-            </svg>
-          </div>
-        </div>
-      </div>
       <div className="logoPictureFooter">
         <img
           key={generalDataImg[0].id}
@@ -90,24 +54,78 @@ const Footer = () => {
           alt={generalDataImg[0].alt}
         />
       </div>
+      <div
+        className="footerContainer"
+        style={{
+          width: "100%",
+          maxWidth: "800px"
+        }}
+      >
+        <div className="socialNetWorkContainer">
+          <div className="socialNetwork">
+            <svg xmlns="http://www.w3.org/2000/svg">
+              <defs>
+                <symbol id="instagram-icon" viewBox="0 0 448 512">
+                  <path d={generalDataImg[4].logoInstagram} />
+                </symbol>
+              </defs>
+              <use xlinkHref="#instagram-icon" />
+            </svg>
+          </div>
+          <div className="socialNetwork">
+            <svg xmlns="http://www.w3.org/2000/svg">
+              <defs>
+                <symbol id="tiktok-icon" viewBox="0 0 512 512">
+                  <path d={generalDataImg[6].logoTikTok} />
+                </symbol>
+              </defs>
+              <use xlinkHref="#tiktok-icon" />
+            </svg>
+          </div>
+          <div className="socialNetwork">
+            <svg xmlns="http://www.w3.org/2000/svg">
+              <defs>
+                <symbol id="facebook-icon" viewBox="0 0 320 512">
+                  <path d={generalDataImg[5].logoFacebook} />
+                </symbol>
+              </defs>
+              <use xlinkHref="#facebook-icon" />
+            </svg>
+          </div>
+        </div>
 
-      <div className="legal">
-        <p
-          onClick={(e) =>
-            handleCategoryChange(e, () => navigate("/privacy"), -105)
-          }
-        >
-          Politique de confidentialité
-        </p>
-        <p
-          onClick={(e) =>
-            handleCategoryChange(e, () => navigate("/terms"), -105)
-          }
-        >
-          Condition d'utilisation
-        </p>
+        <div className="legal">
+          <div className="containerLegal">
+            <p> ©{currentYear} Tous droits réservés </p>
+            <div
+              className="legalArticle"
+              style={{
+                borderTop: "2px solid white",
+                marginTop: "15px"
+              }}
+            >
+              <p
+                style={{
+                  borderRight: "2px solid white"
+                }}
+                onClick={(e) =>
+                  handleCategoryChange(e, () => navigate("/privacy"), -105)
+                }
+              >
+                Politique de confidentialité
+              </p>
+              <p
+                onClick={(e) =>
+                  handleCategoryChange(e, () => navigate("/terms"), -105)
+                }
+              >
+                Condition d'utilisation
+              </p>
+            </div>
+          </div>
+        </div>
+        <p className="contact">Contactez moi</p>
       </div>
-      <div>Contactez moi</div>
     </footer>
   );
 };

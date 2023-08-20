0b4f2431bc555817dc0820fe8643263f5a0f4ae8
Julien Froger <webdev.julienfroger@gmail.com>
2023-08-20 15:57:04 +0200

    update css footer

--- frontend/src/components/data/Footer.js
+++ frontend/src/components/data/Footer.js
@@ -45,6 +45,52 @@ const Footer = () => {
 
   return (
     <footer className="footer">
+      <div className="socialNetWorkContainer">
+        <div className="socialNetwork">
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg">
+              <defs>
+                <symbol id="instagram-icon" viewBox="0 0 448 512">
+                  <path d={generalDataImg[4].logoInstagram} />
+                </symbol>
+              </defs>
+              <use xlinkHref="#instagram-icon" height="20px" />
+            </svg>
+          </div>
+        </div>
+        <div className="socialNetwork">
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg">
+              <defs>
+                <symbol id="tiktok-icon" viewBox="0 0 512 512">
+                  <path d={generalDataImg[6].logoTikTok} />
+                </symbol>
+              </defs>
+              <use xlinkHref="#tiktok-icon" height="20px" />
+            </svg>
+          </div>
+        </div>
+        <div className="socialNetwork">
+          <div>
+            <svg xmlns="http://www.w3.org/2000/svg">
+              <defs>
+                <symbol id="facebook-icon" viewBox="0 0 448 512">
+                  <path d={generalDataImg[5].logoFacebook} />
+                </symbol>
+              </defs>
+              <use xlinkHref="#facebook-icon" height="20px" />
+            </svg>
+          </div>
+        </div>
+      </div>
+      <div className="logoPictureFooter">
+        <img
+          key={generalDataImg[0].id}
+          src={generalDataImg[0].src}
+          alt={generalDataImg[0].alt}
+        />
+      </div>
+
       <div className="legal">
         <p
           onClick={(e) =>
@@ -61,43 +107,7 @@ const Footer = () => {
           Condition d'utilisation
         </p>
       </div>
-      <div className="socialNetwork">
-        <div>
-          <svg xmlns="http://www.w3.org/2000/svg">
-            <defs>
-              <symbol id="instagram-icon" viewBox="0 0 448 512">
-                <path d={generalDataImg[4].logoInstagram} />
-              </symbol>
-            </defs>
-            <use xlinkHref="#instagram-icon" height="20px" />
-          </svg>
-        </div>
-      </div>
-      <div className="socialNetwork">
-        <div>
-          <svg xmlns="http://www.w3.org/2000/svg">
-            <defs>
-              <symbol id="tiktok-icon" viewBox="0 0 512 512">
-                <path d={generalDataImg[6].logoTikTok} />
-              </symbol>
-            </defs>
-            <use xlinkHref="#tiktok-icon" height="20px" />
-          </svg>
-        </div>
-      </div>
-      <div className="socialNetwork">
-        <div>
-          <svg xmlns="http://www.w3.org/2000/svg">
-            <defs>
-              <symbol id="facebook-icon" viewBox="0 0 448 512">
-                <path d={generalDataImg[5].logoFacebook} />
-              </symbol>
-            </defs>
-            <use xlinkHref="#facebook-icon" height="20px" />
-          </svg>
-        </div>
-      </div>
-      <div>Nous contacter</div>
+      <div>Contactez moi</div>
     </footer>
   );
 };

f83862fcad704aabe2fb161ac27fca4e7cb754b6
Julien Froger <webdev.julienfroger@gmail.com>
2023-08-25 10:40:47 +0200

    update heRafraichissant product

--- frontend/src/data/product/HErafraichissant.js
+++ frontend/src/data/product/HErafraichissant.js
@@ -9,39 +9,44 @@ export const heRafraichissant = {
     paragraph: [
       {
         id: "0heRafraichissant",
-        subTitle: "Un stimulant pour votre esprit et un remède naturel",
-        text: "Dans notre monde moderne, nous sommes constamment sollicités par des informations et des distractions. Cela peut rendre difficile le maintien d'une concentration optimale, que ce soit pour le travail, l'apprentissage ou même les tâches quotidiennes. C'est là que notre huile essentielle entre en jeu. Grâce à ses arômes délicats, elle stimule votre esprit, favorisant une meilleure concentration et une clarté mentale accrue. Quelques gouttes dans un diffuseur ou sur un mouchoir peuvent suffire à vous aider à rester concentré et alerte. De plus, notre huile essentielle offre un remède naturel contre les nausées et les maux de tête. Que vous soyez en voyage et que vous souffriez du mal des transports, ou que vous ayez simplement un mal de tête après une longue journée, quelques gouttes de notre huile essentielle peuvent vous offrir un soulagement rapide et efficace."
+        subTitle: "Stimulez votre esprit",
+        text: "Dans notre monde moderne, nous sommes constamment sollicités par des informations et des distractions. Cela peut rendre difficile le maintien d'une concentration optimale, que ce soit pour le travail, l'apprentissage ou même les tâches quotidiennes. C'est là que notre huile essentielle entre en jeu. Grâce à ses arômes délicats, elle stimule votre esprit, favorisant une meilleure concentration et une clarté mentale accrue. De plus, notre huile essentielle offre un remède naturel contre les nausées et les maux de tête. Que vous soyez en voyage et que vous souffriez du mal des transports, ou que vous ayez simplement un mal de tête après une longue journée, quelques gouttes de notre huile essentielle peuvent vous offrir un soulagement rapide et efficace."
       },
       {
         id: "1heRafraichissant",
-        subTitle: "Un confort respiratoire et cutané",
-        text: "La respiration est essentielle à la vie, mais elle peut être entravée par des problèmes tels que la congestion ou les allergies. Notre huile essentielle rafraîchissante est là pour vous aider. Elle apaise les toux, dégage les voies respiratoires et procure une sensation de fraîcheur, vous permettant de respirer librement et facilement. Quelques gouttes dans un diffuseur peuvent aider à purifier l'air et à faciliter la respiration. De plus, notre huile essentielle peut aider à apaiser les démangeaisons, réduire les rougeurs et calmer les piqûres d'insectes. Elle peut être appliquée directement sur la peau ou diluée dans une huile de support pour un soulagement rapide et efficace. De plus, ses propriétés régulatrices aident à contrôler la transpiration excessive, vous permettant de vous sentir plus frais et plus confiant.",
+        subTitle: "Combattez vos maux",
+        text: "La respiration est essentielle à la vie, mais elle peut être entravée par des problèmes tels que la congestion ou les allergies. Notre huile essentielle rafraîchissante est là pour vous aider. Elle apaise les toux, dégage les voies respiratoires et procure une sensation de fraîcheur, vous permettant de respirer librement et facilement. De plus, notre huile essentielle peut aider à apaiser les démangeaisons, réduire les rougeurs et calmer les piqûres d'insectes. Elle peut être appliquée directement sur la peau ou diluée dans une huile de support pour un soulagement rapide et efficace. De plus, ses propriétés régulatrices aident à contrôler la transpiration excessive, vous permettant de vous sentir plus frais et plus confiant.",
         picture450: "/img/jpg/product/HErafraichssantIllusText450.jpg",
         picture675: "/img/jpg/product/HErafraichssantIllusText675.jpg",
         picture900: "/img/jpg/product/HErafraichssantIllusText900.jpg"
       },
       {
         id: "2heRafraichissant",
-        subTitle: "Un soulagement pour les douleurs et les tensions",
+        subTitle: "Soulagez vos douleurs et vos tensions",
         text: "Le stress et la tension peuvent s'accumuler dans notre corps, causant des douleurs et des malaises. Notre huile essentielle peut aider à soulager ces symptômes. Qu'il s'agisse de douleurs articulaires, de tensions musculaires ou de maux de tête, quelques gouttes de notre huile essentielle peuvent aider à soulager la douleur et à favoriser la relaxation. Elle peut être massée directement sur les zones douloureuses ou utilisée dans un bain chaud pour un effet apaisant global."
       },
       {
         id: "3heRafraichissant",
-        subTitle: "Un soutien pour les femmes ménopausées et les sportifs",
-        text: "La ménopause est une étape naturelle de la vie d'une femme, mais elle peut s'accompagner de symptômes inconfortables tels que les bouffées de chaleur. Notre huile essentielle peut aider à réduire ces symptômes, procurant un soulagement naturel et un confort pendant cette étape de la vie. Quelques gouttes sur un mouchoir ou dans un diffuseur peuvent aider à apaiser les bouffées de chaleur et à favoriser un sentiment de bien-être. De plus, que vous soyez un athlète professionnel ou que vous aimiez simplement faire de l'exercice pour rester en forme, notre huile essentielle peut aider à soulager les douleurs musculaires et à favoriser une récupération plus rapide. Elle peut être appliquée directement sur les muscles endoloris ou utilisée dans un bain chaud après l'exercice pour un effet apaisant."
+        subTitle: "Pour vous, Mesdames",
+        text: "La ménopause est une étape naturelle de la vie d'une femme, mais elle peut s'accompagner de symptômes inconfortables tels que les bouffées de chaleur. Notre huile essentielle peut aider à réduire ces symptômes, procurant un soulagement naturel et un confort pendant cette étape de la vie. Quelques gouttes peuvent aider à apaiser les bouffées de chaleur et à favoriser un sentiment de bien-être."
       },
       {
         id: "4heRafraichissant",
+        subTitle: "Pour les sportifs",
+        text: "Que vous soyez un athlète professionnel ou que vous aimiez simplement faire de l'exercice pour rester en forme, notre huile essentielle peut aider à soulager les douleurs musculaires et à favoriser une récupération plus rapide. Elle peut être appliquée directement sur les muscles endoloris ou utilisée dans un bain chaud après l'exercice pour un effet apaisant."
+      },
+      {
+        id: "5heRafraichissant",
         subTitle: "Revitalisez votre quotidien avec notre huile essentielle",
         text: "Notre huile essentielle offre une multitude de bienfaits pour votre bien-être. Que vous cherchiez à améliorer votre concentration, à soulager des douleurs, à respirer plus facilement ou à apaiser votre peau, elle peut vous aider à atteindre vos objectifs. Profitez des bienfaits variés de cette huile essentielle pour revitaliser votre quotidien et embrasser une vie plus épanouissante. Quelques gouttes peuvent faire une grande différence dans votre journée, vous aidant à vous sentir plus équilibré, plus concentré et plus en paix."
       },
       {
-        id: "5heRafraichissant",
+        id: "6heRafraichissant",
         subTitle: "Vous aimerez aussi…",
         text: ""
       },
       {
-        id: "6heRafraichissant",
+        id: "7heRafraichissant",
         subTitle: "",
         text: ""
       }
@@ -58,15 +63,17 @@ export const heRafraichissant = {
         id: "heRafraichissant1",
         link: "pb",
         pictureLove: "/img/jpg/product/PB.jpg",
-        textLove:
-          "Je conseille vivement aux sportifs d'explorer notre produit destiné à la récupération musculaire et à l'amélioration des performances, qui offre une énergie stimulante à base de cacao."
+        textLove: [
+          "Je conseille vivement aux sportifs d'explorer notre produit destiné à la récupération musculaire et à l'amélioration des performances, qui offre une énergie stimulante à base de cacao.",
+          "Il est également un très bon booster d’énergie naturelle pour tenir toute la journée."
+        ]
       },
       {
         id: "heRafraichissant2",
         link: "mereDeTous",
         pictureLove: "/img/jpg/product/mereDeTous.jpg",
         textLove:
-          "Elle s'associe parfaitement à notre anti-inflammatoire naturel, offrant ainsi une solution complète pour gérer les douleurs liées à l'activité physique."
+          "Elle s'associe parfaitement à notre anti-inflammatoire naturel, offrant ainsi une solution complète pour gérer les douleurs liées à l'activité physique ou au quotidien lors de pathologies douloureuses."
       },
       {
         id: "heRafraichissant3",
@@ -109,28 +116,41 @@ export const heRafraichissant = {
   inSummary: [
     {
       id: "HERaiS0",
-      title: "Gestion du poids",
-      text: "Détoxifiant"
+      title: "Santé",
+      text: (
+        <ul>
+          <li>
+            Efficace pour les maladies saisonnières (décongestionnant) et les
+            allergies
+          </li>
+          <li>
+            Agit rapidement sur les migraines, nausées ainsi que le mal des
+            transports
+          </li>
+          <li>
+            Diminue les douleurs et tensions musculaires et articulaires
+            (foulures, entorses, douleurs de dos…)
+          </li>
+          <li>
+            Atténue les rougeurs et démangeaisons telles que les piqûres
+            d'insectes
+          </li>
+        </ul>
+      )
     },
     {
       id: "HERaiS1",
-      title: "Problèmes cutanés",
-      text: "Il élimine les aphtes, les boutons de fièvre, les verrues et l’eczéma."
-    },
-    {
-      id: "HERaiS2",
-      title: "Santé",
-      text: "Il soutient le système immunitaire et agit contre les douleurs musculaires, articulaires, osseuses, douleurs de menstruation et symptômes de la ménopause. Il aide à diminuer le cholestérol et la glycémie. Il est recommandé sur les pathologies telles que la dépression, l’arthrite, l’ostéoporose, les rhumatismes…"
-    },
-    {
-      id: "HERaiS3",
-      title: "Beauté ",
-      text: "Il augmente la production de l’hormone anti-âge et contient des ingrédients qui luttent contre le vieillissement cutané et la chute des cheveux ."
-    },
-    {
-      id: "HERaiS4",
       title: "Bien-être",
-      text: "Il apaise en agissant directement sur l’hormone du stress. Il diminue la fatigue et améliore le sommeil pour une sensation de détente assurée."
+      text: (
+        <ul>
+          <li>Relaxe le corps et l’esprit</li>
+          <li>Aide à la concentration et à la clarté mentale</li>
+          <li>
+            Recommandée pour les femmes en période de ménopause afin de lutter
+            contre les bouffées de chaleur
+          </li>
+        </ul>
+      )
     }
   ]
 };
